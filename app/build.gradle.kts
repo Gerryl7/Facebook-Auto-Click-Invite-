d13eaea56fd43bf35aa3882cec0e91e75b71ed86
Gerryl Destor <jimsdestor14@gmail.com>
2026-09-24 09:09:13 +0800

    Add Facebook Invite Auto-Scroller Android project

---
+++ app/build.gradle.kts
@@ -0,0 +1,4 @@
+plugins { id("com.android.application"); id("org.jetbrains.kotlin.android") }
+android { namespace="com.example.fbinviteautoscroller"; compileSdk=35
+ defaultConfig { applicationId="com.example.fbinviteautoscroller"; minSdk=24; targetSdk=34; versionCode=1; versionName="1.0" } }
+dependencies { implementation("androidx.core:core-ktx:1.15.0"); implementation("androidx.appcompat:appcompat:1.7.0") }
